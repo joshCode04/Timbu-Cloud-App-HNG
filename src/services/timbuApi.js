010840ac606a42cccb7be6af9cc111eccd940947
Olumide04 <ayeromara12@gmail.com>
2024-07-14 10:06:29 +0100

    commit

--- src/services/timbuApi.js
+++ src/services/timbuApi.js
@@ -1,7 +1,7 @@
 export const getProducts = async () => {
   try {
     const response = await fetch(
-      "/api/products?organization_id=0de666116e3f462091bde71fb3640dc9&Appid=Q2RRZW5O443IO7F&Apikey=f96b546f72af4dd58c1e9ffe6cd7e39520240712185804901395",
+      "https://api.timbu.cloud/products?organization_id=0de666116e3f462091bde71fb3640dc9&Appid=Q2RRZW5O443IO7F&Apikey=f96b546f72af4dd58c1e9ffe6cd7e39520240712185804901395",
       {
         method: "GET",
         headers: {
